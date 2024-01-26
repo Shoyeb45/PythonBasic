ec76b28e83acfdd79b6d809e3fc0d97daf69d3b5
Shoyeb45 <shoyebff45@gmail.com>
2024-01-26 15:31:15 +0530

    strign methods

--- String/slicing.py
+++ String/slicing.py
@@ -8,7 +8,12 @@ print(str[: :2])
 
 str = "My Dear Student"
 print(str)
-print(str[-9: :-1 ])
+print(str[-9: :-1 ]) 
 
 # reversing the string
 print(str[: :-1])
+
+# will not print anything
+print(str[1:6:-1])
+
+print(str[9:1:-1])  # reverse of 'Dear St'
