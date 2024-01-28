b18f133b54ad2e4ec89cf516a73769736cfc3fcb
Shoyeb45 <shoyebff45@gmail.com>
2024-01-28 15:12:51 +0530

    functions

---
+++ Functions/function.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the 2 number:\n"))
+b = int(input())
+
+# function for finding maximum number between two numbers
+
+def Max(a, b):
+    if(a > b):
+        print("The maximum number is",a)
+    else:
+        print("The maximum number is",b)
+
+Max(a, b)   # Function call
