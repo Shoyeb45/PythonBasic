c57af097b85d4d15cd0f66e0e12e7f7e8ecdeb5f
Shoyeb45 <shoyebff45@gmail.com>
2024-01-29 12:19:05 +0530

    list

---
+++ List/intro.py
@@ -0,0 +1,17 @@
+import sys
+
+#  declaring list
+l1 = [1, 2, 3, 5, 67, 0]
+
+# also
+
+l2 = list() # empty list
+l3 = list([1, 4, complex(9, 5), 23, 19, -98])
+l4 = list("Shoyeb Ansari")  # each character assigne to list
+
+a = 1 
+print(id(a))
+print(id(l1[0]))
+print(len(l2))
+
+print(l3[1: -3])
