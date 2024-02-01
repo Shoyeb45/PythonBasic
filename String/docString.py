8a5b3e078787736e46d80a5430d3be69fd0af1e9
Shoyeb45 <shoyebff45@gmail.com>
2024-02-01 15:55:41 +0530

    doc and f string

---
+++ String/docString.py
@@ -0,0 +1,6 @@
+def square(n):
+    '''The square of n'''
+    print(n ** 2)
+
+square(5)
+print(square.__doc__)
---
+++ String/fstring.py
@@ -0,0 +1,9 @@
+txt = "The price is {price:.2f} dollars"
+print(txt.format(price = 453))
+
+name = "shoyeb"
+age = 19
+print(f"My name is {name} and my age is {age}")
+
+val = 'Shoyeb'  
+print(f"My name is {val} and I am {val}.")
