54c569a25a82fa60bfe3183a496a3a4c636ae98d
Shoyeb45 <shoyebff45@gmail.com>
2024-02-03 19:29:27 +0530

    dictionary methods

--- Dictionary/methods.py
+++ Dictionary/methods.py
@@ -1,6 +1,4 @@
-# 1. del
-
-d = {
+info = {
     "Name" : "Mitesh",
     "DOB"  : "28 May, 2004",
     "Profession" : "Student",
@@ -8,5 +6,33 @@ d = {
     "Dropper" : False
 }
 
-del d["Age"]
-print(d)
+
+# 1. del
+del info["Age"]
+print(info)
+
+# 2. update()
+info.update({"Dropper":True})
+info.update({"Age":"19"})
+print(info)
+
+# 3.clear
+info.clear()
+print(info)
+
+# 4.pop
+
+book = {
+    "Title" : "Harry Potter and order of the phoenix",
+    "Author" : "J K Rowling",
+    "Genre" : "Fantasy",
+    "Price" : "₹449.00/-",
+    "Pages" : "766 pages"
+}
+
+book.pop("Pages")
+print(book)
+
+# 5. popitem
+book.popitem()
+print(book)
