55da172afe2b8cb1a88d92bf8a9ba1600683fbb7
Shoyeb45 <shoyebff45@gmail.com>
2024-02-04 15:26:15 +0530

    recursion

--- Functions/recursion.py
+++ Functions/recursion.py
@@ -1,10 +1,10 @@
 # function to calculate fatorial of a number
 
 def factorial(n):
-    if(n == 1 or n == 0):
+    if(n == 1 or n == 0):   # base condition
         return 1
     else:
-        return n*factorial(n-1)
+        return n*factorial(n-1)   # recursive statement
 
 # calling function
 
@@ -13,10 +13,17 @@ print(factorial(a))
 
 # let's calculate fibonacci series upto n-terms
 
-def fibonacci(n, a):
-    if( a == 0 or a == 1):
-        print(a," ")
-        a=a+1
+def fibonacci(a, b, n):
+    if n == 0:
+        return     # base condition
     else:
-        
+        print(a+b," ")   # printing from third term
+        fibonacci(b, a+b, n-1)   # function call with new term and decreasing n
+
+
+a = 0
+b = 1
+n = int(input("Enter the number of terms:"))
+print(a,"\n",b)
+fibonacci(a, b, n-2)
 
