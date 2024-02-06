f6d4c3b0383fb071366f989e0bcc82e560ac9b13
Shoyeb45 <shoyebff45@gmail.com>
2024-02-06 17:14:28 +0530

    import

---
+++ Basic/import.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import turtle
+from math import sqrt, pi
+
+print(f"{sqrt(144)}, pi = {pi} , pi * sqrt(144) = {sqrt(144)} * {pi}")
+
+print(f"Turtle all functions and methods:\n{dir(turtle)}")
+print(f"Pandas all functions and methods:\n{dir(pd)}")
