b09b1604e51a78d3127e37a8dbedba18c9ce5aa9
Shoyeb45 <shoyebff45@gmail.com>
2024-02-09 13:45:12 +0530

    loops

---
+++ Loops/somePatterns.py
@@ -0,0 +1,14 @@
+for i in range(5):
+    for j in range(5):
+        print("#", end=' ')
+    print()
+
+
+for k in range(5):
+    for p in range(5):
+        if( p <= k):
+            print("#", end='=')
+    print()
+
+a = int(input("Enter the number:"))
+
