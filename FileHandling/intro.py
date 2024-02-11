1f45576f1fccb049421ba9001e3b3a0857244f52
Shoyeb45 <shoyebff45@gmail.com>
2024-02-11 10:42:09 +0530

    file handling

--- FileHandling/intro.py
+++ FileHandling/intro.py
@@ -1,4 +1,27 @@
 # opening a file
 
-file = open('requirement.txt', 'w' )
-print(file)
+file = open('requirement.txt', 'r' )
+# print(file)
+
+# accessing file content
+text = file.read()
+print(text)
+file.close()
+
+w = open('requirement.txt', 'w')
+
+w.write("This file contains very important information, so please don't try to peek in file.")
+w.close()
+
+# creating new file
+
+# newFile = open("Hello.txt", "x")
+newFile = open("Hello.txt", "a")
+
+# appending to a file
+newFile.write("Hello, how are you? ")
+newFile.close()
+
+
+with  open("Info.txt", 'w') as t:
+    t.write("Python is very useful programming language.")
