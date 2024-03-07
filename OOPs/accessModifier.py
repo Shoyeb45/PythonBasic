eb647883aa11b8b70d529688cff00462627dec6a
Shoyeb45 <shoyebff45@gmail.com>
2024-03-07 13:01:45 +0530

    access modifier

---
+++ OOPs/accessModifier.py
@@ -0,0 +1,30 @@
+# Two types of access modifier(in python):
+# 1. Public - available to everyone
+# 2. Private - Internally
+
+class Cricket:
+    def __init__(self, player1, player2, fight):
+        self.player1 = player1
+        self.player2 = player2
+        # Making private variable
+        self.__fight = fight
+
+    def play(self):
+        print("I am copy crickekt champion")
+        print(f"Private variable: {self.__fight}")  # we can access from the instance method because it is internal
+        #  we can call private method internally
+        self.__internalConvo()
+
+    # Defining private method
+    def __internalConvo(self):
+        print(f"{self.player1} you just missed a stump today")
+
+cric = Cricket("MS Dhoni", "Virat Kohli", "Rare")
+
+print(cric.player1) # These are public variable
+# print(cric.__fight)  => This will throw error :  AttributeError
+
+cric.play()
+
+# Accwessing private method
+# cric.__internalConvo()  => It will throw error
