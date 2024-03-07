30dc2bf519e1a2621e40bbd7336c80cf199d5fdb
Shoyeb45 <shoyebff45@gmail.com>
2024-03-07 13:05:14 +0530

    inheritence

---
+++ OOPs/inheritence.py
@@ -0,0 +1,18 @@
+# 
+
+class Animal:
+    # def __init__(self, animal1):
+    #     self.animal1 = animal1
+
+    def eat(self):
+        print("Animal is eating")
+
+
+class Dog(Animal):
+        pass
+
+
+dog = Dog()
+dog.eat()
+
+    
