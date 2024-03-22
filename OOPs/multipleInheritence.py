93d47b040a02af8e1be041d274331614cf2da019
Shoyeb45 <shoyebff45@gmail.com>
2024-03-22 14:38:23 +0530

    multiple inheritence

---
+++ OOPs/multipleInheritence.py
@@ -0,0 +1,31 @@
+# Diamond problem - If we have two parent method A and B, and C is child method of A and B -> C(A, B). And we have some method like play(), which is present in all the A, B and C. Then we call the method as instance of C, then in following way, the search will be followed:
+# First it will search in C, and if there it is not present then it will go to that class which we have provided first in argument like C(A, B), here the search will go to the A.
+# And then B.
+# This method is called MRO Approach (Method Resolution Order).
+
+# Example
+
+class A:
+    def __init__(self, name):
+        self.name = name
+
+    def play(self):
+        print("I am A.") 
+
+class B:
+    def __init__(self, name):
+        self.name = name
+
+    def play(self):
+        print("I am B.") 
+
+class C(B, A):
+    pass
+# class C(A, B):
+#     pass
+
+c = C("Shoyeb")
+c.play()      
+
+# DFS - Depth First Search 
+# Internally it uses above mentioned algorithm.
