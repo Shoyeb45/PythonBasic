c9bdd1fd5d193189280e7d71ca7675dbe9e34388
Shoyeb45 <shoyebff45@gmail.com>
2024-03-22 14:38:36 +0530

    types of inheritence

---
+++ OOPs/typesOfInheritence.py
@@ -0,0 +1,6 @@
+#                       5 types of inheritence:
+# 1. Single Inheritence (A -> B)
+# 2. Multiple Inheritence (A -> C, B -> C)
+# 3. Multiline Inheritence  (A -> B -> C)
+# 4. Hierarchial Inheritence (A -> B, A -> C)
+# 5. Hybrid Inheritence (A -> C, B -> C, C -> D, C -> E)
