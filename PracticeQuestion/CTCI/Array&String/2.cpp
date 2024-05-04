ebd08a784b5f63fc860b29a2ac2d96c989da5405
Shoyeb45 <shoyebff45@gmail.com>
2024-05-04 09:04:00 +0530

    URLify and permutation

--- PracticeQuestion/CTCI/Array&String/2.cpp
+++ PracticeQuestion/CTCI/Array&String/2.cpp
@@ -15,7 +15,26 @@ bool isPermutation(string orig, string perm) {
     if(orig == perm)
         return true;
     return false;
-    // TC: O(|orig|)
+    // TC: O(|orig| * log(|orig|))
+}
+
+bool isPerm(string a, string b) {
+    map<char, int> m1;
+    for(char ch: a)
+        m1[ch]++;
+    
+    map<char, int> m2;
+    for(char ch: b)
+        m2[ch]++;
+
+    if(m1.size() != m2.size())
+            return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(m1[a[i]] != m2[a[i]])
+            return false;
+    }
+
+    return true;
 }
 int main() {
     ios_base::sync_with_stdio(false);
@@ -24,7 +43,11 @@ int main() {
     cin >> t;
 
     while (t--) {
-        
+        string s, a; cin >> s >> a;
+        if(isPermutation(s, a)) 
+            cout << "Strings are permutation of one another\n";
+        else
+            cout << "Strings are not permutation\n";
     }
     
     return 0;
---
+++ PracticeQuestion/CTCI/Array&String/3.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+typedef long long ll;
+const ll mod = 1000000007;
+
+vector<string> split(string s, char x) {
+    vector<string> ans;
+    string a = "";
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == x) {
+            ans.push_back(a);
+            a = "";
+        } else {
+            a += s[i];
+        }
+        if(i == s.size() - 1) {
+            ans.push_back(a);
+        }
+    }
+    return ans;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    getline(cin, s);
+    vector<string> words = split(s, ' ');
+
+    string ans = words[0];
+    for(int i = 1; i < words.size(); i++) {
+        ans = ans + "%20" + words[i];
+    }
+    cout << ans;
+    return 0;
+}
