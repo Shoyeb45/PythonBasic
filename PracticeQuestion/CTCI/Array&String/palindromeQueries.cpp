514fc56d2bb27dd5e8e12f566592031804ae3bad
Shoyeb45 <shoyebff45@gmail.com>
2024-05-07 00:05:26 +0530

    Palindrome queries

---
+++ PracticeQuestion/CTCI/Array&String/palindromeQueries.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i, n) for(int i = 0; i < n; i++)
+typedef long long ll;
+const ll mod = 1000000007;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, q; cin >> n >> q;
+    string s; cin >> s;
+
+    while (q--)
+    {
+        int l, r;
+        cin >> l >> r;
+        
+    }
+    
+    
+    return 0;
+}
